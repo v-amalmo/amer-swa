c573424f5f10b180895ebb5294589387e83ef89c
Amer AlMotlaq <v-amalmo@microsoft.com>
2021-07-04 09:34:16 +0300

    04-07-2021 9:34 AM

---
+++ api/HttpCallback.cs
@@ -0,0 +1,32 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace Company.Function
+{
+    public static class HttpCallback
+    {
+        [FunctionName("HttpCallback")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            string name = req.Query["name"];
+
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();            
+            log.LogInformation(requestBody);
+
+            dynamic data = JsonConvert.DeserializeObject(requestBody);            
+            
+            return new OkObjectResult(data);
+        }
+    }
+}
